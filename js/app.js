3282d7c5fd47ef6a0f0ffd795005235b3b418e22
reavensf <rhfenelon@gmail.com>
2017-04-19 10:41:14 -0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+// Quiz State object
+var quizState = {
+   
+}
+
+var questions = [
+    {
+        questionOne: 'In music, what instrument keeps the rythmn and tempo?',
+        questionOneChoiceA: 'a) Piano',
+        questionOneChoiceB: 'b) Drums',
+        questionOneChoiceC: 'c) Bass Guitar',
+        questionOneChoiceD: 'd) Metronome'
+    },
+    {
+        questionTwo: 'What musical instrument can allow you to have a great workout while playing?',
+        questionTwoChoiceA: 'a) Drums',
+        questionTwoChoiceB: 'b) Guitar',
+        questionTwoChoiceC: 'c) Triangle',
+        questionTwoChoiceD: 'd) Zylophone',
+    },
+    {
+        questionThree: 'What part of the drumset can act as the metronome during a song?',
+        questionThreeChoiceA: 'a) Snare',
+        questionThreeChoiceB: 'b) Kick Drum',
+        questionThreeChoiceC: 'c) Cymbals',
+        questionThreeChoiceD: 'd) Hi-hats', 
+    },
+    {
+        questionFour: 'What is the name of this drum piece?',
+        questionFourImage: '<img src="">',
+        questionFourChoiceA: 'a) Tom',
+        questionFourChoiceB: 'b) Hi-Hat',
+        questionFourChoiceC: 'c) Snare',
+        questionFourChoiceD: 'd) Kick Drum', 
+    },
+    {
+        questionFive: 'Which parts of a drumset do you use your feet to make/alter a sound?',
+        questionFiveChoiceA: 'a) Hi-Hat and Kick Drum',
+        questionFiveChoiceB: 'b) Kick drum and Snare',
+        questionFiveChoiceC: 'c) Hit-Hats and Toms',
+        questionFiveChoiceD: 'd) Kick Drum and Cymbals', 
+    },
+    {
+        questionFive: 'What is the name of this piece?',
+        questionFiveChoiceA: 'a) Drum Seat',
+        questionFiveChoiceB: 'b) Drum Chair',
+        questionFiveChoiceC: 'c) Drum Stool',
+        questionFiveChoiceD: 'd) Drum Throne', 
+    },
+    {
+        questionFive: 'What is the best instrumnet to play in the world?',
+        questionFiveChoiceA: 'a) Drums',
+        questionFiveChoiceB: 'b) Piano',
+        questionFiveChoiceC: 'c) Electric Guitar',
+        questionFiveChoiceD: 'd) Bass Guitar', 
+    }
+]
+
+
+// 
+var  = function(){
+    
+}
+
+// 
+var  = function(){
+    
+}
