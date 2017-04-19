ca8fb8b5beb72af0d688fcc7f3e2f38a58cc2438
reavensf <rhfenelon@gmail.com>
2017-04-19 13:06:27 -0400

    Progress so far

--- js/app.js
+++ js/app.js
@@ -5,64 +5,95 @@ var quizState = {
 
 var questions = [
     {
-        questionOne: 'In music, what instrument keeps the rythmn and tempo?',
-        questionOneChoiceA: 'a) Piano',
-        questionOneChoiceB: 'b) Drums',
-        questionOneChoiceC: 'c) Bass Guitar',
-        questionOneChoiceD: 'd) Metronome'
+        question: 'In music, what instrument keeps the rythmn and tempo?',
+        questionChoiceA: 'Piano',
+        questionChoiceB: 'Drums',
+        questionChoiceC: 'Bass Guitar',
+        questionChoiceD: 'Metronome'
     },
     {
-        questionTwo: 'What musical instrument can allow you to have a great workout while playing?',
-        questionTwoChoiceA: 'a) Drums',
-        questionTwoChoiceB: 'b) Guitar',
-        questionTwoChoiceC: 'c) Triangle',
-        questionTwoChoiceD: 'd) Zylophone',
+        question: 'What musical instrument can allow you to have a great workout while playing?',
+        questionChoiceA: 'Drums',
+        questionChoiceB: 'Guitar',
+        questionChoiceC: 'Triangle',
+        questionChoiceD: 'Zylophone',
     },
     {
-        questionThree: 'What part of the drumset can act as the metronome during a song?',
-        questionThreeChoiceA: 'a) Snare',
-        questionThreeChoiceB: 'b) Kick Drum',
-        questionThreeChoiceC: 'c) Cymbals',
-        questionThreeChoiceD: 'd) Hi-hats', 
+        question: 'What part of the drumset can act as the metronome during a song?',
+        questionChoiceA: 'Snare',
+        questionChoiceB: 'Kick Drum',
+        questionChoiceC: 'Cymbals',
+        questionChoiceD: 'Hi-hats', 
     },
     {
-        questionFour: 'What is the name of this drum piece?',
-        questionFourImage: '<img src="">',
-        questionFourChoiceA: 'a) Tom',
-        questionFourChoiceB: 'b) Hi-Hat',
-        questionFourChoiceC: 'c) Snare',
-        questionFourChoiceD: 'd) Kick Drum', 
+        question: 'What is the name of this drum piece?',
+        questionImage: '<img src="img/snare-drum.jpg">',
+        questionChoiceA: 'Tom',
+        questionChoiceB: 'Hi-Hat',
+        questionChoiceC: 'Snare',
+        questionChoiceD: 'Kick Drum', 
     },
     {
-        questionFive: 'Which parts of a drumset do you use your feet to make/alter a sound?',
-        questionFiveChoiceA: 'a) Hi-Hat and Kick Drum',
-        questionFiveChoiceB: 'b) Kick drum and Snare',
-        questionFiveChoiceC: 'c) Hit-Hats and Toms',
-        questionFiveChoiceD: 'd) Kick Drum and Cymbals', 
+        question: 'Which parts of a drumset do you use your feet to make/alter a sound?',
+        questionChoiceA: 'Hi-Hat and Kick Drum',
+        questionChoiceB: 'Kick drum and Snare',
+        questionChoiceC: 'Hit-Hats and Toms',
+        questionChoiceD: 'Kick Drum and Cymbals', 
     },
     {
-        questionFive: 'What is the name of this piece?',
-        questionFiveChoiceA: 'a) Drum Seat',
-        questionFiveChoiceB: 'b) Drum Chair',
-        questionFiveChoiceC: 'c) Drum Stool',
-        questionFiveChoiceD: 'd) Drum Throne', 
+        question: 'What is the name of this piece?',
+        questionImage: '<img src="img/drum-throne.jpg">',
+        questionChoiceA: 'Drum Seat',
+        questionChoiceB: 'Drum Chair',
+        questionChoiceC: 'Drum Stool',
+        questionChoiceD: 'Drum Throne', 
     },
     {
-        questionFive: 'What is the best instrumnet to play in the world?',
-        questionFiveChoiceA: 'a) Drums',
-        questionFiveChoiceB: 'b) Piano',
-        questionFiveChoiceC: 'c) Electric Guitar',
-        questionFiveChoiceD: 'd) Bass Guitar', 
+        question: 'What is the best instrumnet to play in the world?',
+        questionChoiceA: 'Drums',
+        questionChoiceB: 'Piano',
+        questionChoiceC: 'Electric Guitar',
+        questionChoiceD: 'Bass Guitar', 
     }
-]
+];
+
+// var getQuestion = function(questionIndex){
+//     var questionIndex = 0;
+//     var firstQuestion = Object.values(questions[0].questionOne).join('');
+// };
 
 
-// 
-var  = function(){
+
+$(document).ready(function(){
+    var questionIndex = 0;
+
+    var questionAndChoices = function(){
+        var questionTitle      = Object.values(questions[questionIndex].question).join('');
+        var questionChoiceA    = Object.values(questions[questionIndex].questionChoiceA).join('');
+        var questionChoiceB    = Object.values(questions[questionIndex].questionChoiceB).join('');
+        var questionChoiceC    = Object.values(questions[questionIndex].questionChoiceC).join('');
+        var questionChoiceD    = Object.values(questions[questionIndex].questionChoiceD).join('');
+
+        $('.questionTitle').text(questionTitle);
+        $('.inputA').text(questionChoiceA);
+        $('.inputB').text(questionChoiceB);
+        $('.inputC').text(questionChoiceC);
+        $('.inputD').text(questionChoiceD);
+    }
+    questionAndChoices();
     
-}
 
-// 
-var  = function(){
-    
-}
+    $('#drumQuiz').submit(function(event){
+        event.preventDefault();
+
+        if (questionIndex === 6){
+            questionIndex = 0;
+            return questionAndChoices();
+        } else {
+            questionIndex++
+            return questionAndChoices();
+        }
+        
+        console.log('Submit');
+    });
+});
