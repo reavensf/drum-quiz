21bc11061b001b2bba510ea3ad2e8e76d12b7af7
reavensf <rhfenelon@gmail.com>
2017-04-24 20:55:41 -0400

    Final version with everything wroking

--- js/app.js
+++ js/app.js
@@ -5,66 +5,38 @@ var quizState = {
         {
             question: 'In music, what instrument keeps the rythmn and tempo?',
             choices: ['Piano','Drums','Bass Guitar', 'Metronome'],
-            questionChoiceA: 'Piano',
-            questionChoiceB: 'Drums',
-            questionChoiceC: 'Bass Guitar',
-            questionChoiceD: 'Metronome',
             correctAnswer: 'Drums',
         },
         {
             question: 'What musical instrument can allow you to have a great workout while playing it?',
             choices: ['Drums','Guitar','Triangle', 'Zylophone'],
-            questionChoiceA: 'Drums',
-            questionChoiceB: 'Guitar',
-            questionChoiceC: 'Triangle',
-            questionChoiceD: 'Zylophone',
             correctAnswer: 'Drums'
         },
         {
             question: 'What part of the drumset can act as the metronome during a song?',
             choices: ['Snare','Kick Drum','Cymbals', 'Hi-hats'],
-            questionChoiceA: 'Snare',
-            questionChoiceB: 'Kick Drum',
-            questionChoiceC: 'Cymbals',
-            questionChoiceD: 'Hi-hats',
             correctAnswer: 'Hi-hats' 
         },
         {
             question: 'What is the name of this drum piece?',
             questionImage: '<img src="img/snare-drum.jpg">',
             choices: ['Tom','Hi-Hat','Snare', 'Kick Drum'],
-            questionChoiceA: 'Tom',
-            questionChoiceB: 'Hi-Hat',
-            questionChoiceC: 'Snare',
-            questionChoiceD: 'Kick Drum',
             correctAnswer: 'Snare' 
         },
         {
             question: 'Which parts of a drumset do you use your feet to make/alter a sound?',
             choices: ['Hi-Hat and Kick Drum','Kick drum and Snare','Hit-Hats and Toms', 'Kick Drum and Cymbals'],
-            questionChoiceA: 'Hi-Hat and Kick Drum',
-            questionChoiceB: 'Kick drum and Snare',
-            questionChoiceC: 'Hit-Hats and Toms',
-            questionChoiceD: 'Kick Drum and Cymbals', 
             correctAnswer: 'Hi-Hat and Kick Drum'
         },
         {
             question: 'What is the name of this piece?',
             questionImage: '<img src="img/drum-throne.jpg">',
             choices: ['Drum Seat','Drum Chair','Drum Stool', 'Drum Throne'],
-            questionChoiceA: 'Drum Seat',
-            questionChoiceB: 'Drum Chair',
-            questionChoiceC: 'Drum Stool',
-            questionChoiceD: 'Drum Throne', 
             correctAnswer: 'Drum Throne'
         },
         {
             question: 'What is the best instrumnet to play in the world?',
             choices: ['Drums','Piano','Electric Guitar', 'Bass Guitar'],
-            questionChoiceA: 'Drums',
-            questionChoiceB: 'Piano',
-            questionChoiceC: 'Electric Guitar',
-            questionChoiceD: 'Bass Guitar', 
             correctAnswer: 'Drums'
         }
     ],
@@ -86,37 +58,35 @@ var displayResults = function(){
     var answersCorrect      = quizState.score;
     var numberOfQuestions   = quizState.questions.length;
 
-    var quizScore = Math.ceil((answersCorrect / numberOfQuestions ) * 100) + '%';
+    var quizScore = Math.ceil((answersCorrect / numberOfQuestions ) * 100);
 
     if (quizScore > 50){
-        var overFiftyMessage = 'Congratulations! You got ' + answersCorrect + ' out of ' + numberOfQuestions + ' answers correct. Your score is: ';
+        var overFiftyMessage = 'Congratulations! You got <strong>' + answersCorrect + '</strong> out of <strong>' + numberOfQuestions + '</strong> answers correct. Your score is: ';
 
-        $('.endMessage').text(overFiftyMessage);
+        $('.endMessage').html(overFiftyMessage);
+        $('.results').addClass('greenText');
     } else {
-        var underFiftyMessage = 'Good job. You got ' + answersCorrect + ' out of ' + numberOfQuestions + ' answers correct. Your score is: ';
+        var underFiftyMessage = 'Not bad. You got <strong>' + answersCorrect + '</strong> out of <strong>' + numberOfQuestions + '</strong> answers correct. Your score is: ';
 
-        $('.endMessage').text(underFiftyMessage);
+        $('.endMessage').html(underFiftyMessage);
+        $('.results').addClass('yellowText');
     }
 
-    $('.results').text(quizScore);
+    $('.results').text(quizScore + '%');
 }
 
 var renderQuiz = function(quizState){
     var questionTitle      = quizState.questions[quizState.questionIndex].question;
-    var questionChoiceA    = quizState.questions[quizState.questionIndex].questionChoiceA;
-    var questionChoiceB    = quizState.questions[quizState.questionIndex].questionChoiceB;
-    var questionChoiceC    = quizState.questions[quizState.questionIndex].questionChoiceC;
-    var questionChoiceD    = quizState.questions[quizState.questionIndex].questionChoiceD;
 
     $('.questionTitle').text(questionTitle);
-    $('.inputA').text(questionChoiceA);
-    $('.inputA').parent().find('input').val(questionChoiceA);
-    $('.inputB').text(questionChoiceB);
-    $('.inputB').parent().find('input').val(questionChoiceB);
-    $('.inputC').text(questionChoiceC);
-    $('.inputC').parent().find('input').val(questionChoiceC);
-    $('.inputD').text(questionChoiceD);
-    $('.inputD').parent().find('input').val(questionChoiceD);
+
+    var choicesArray = quizState.questions[quizState.questionIndex].choices;
+    
+    var choicesHtml = choicesArray.map(function(choice){
+        return '<div class="inputWrapper"> <input type="radio" name="answer" value="' + choice + '"> <span>' + choice + '</span> </div>';
+    })
+
+    $('.answersWrapper').html(choicesHtml);
 
     if(quizState.questionIndex === 3 || quizState.questionIndex === 5){
         var questionImage = quizState.questions[quizState.questionIndex].questionImage;
@@ -144,13 +114,14 @@ var addQuestionProgress = function(quizState){
     $('.numberOfQuestions').text(numberOfQuestions);
 }
 
+var hideNextShowSubmit = function(){
+    $('input[type=submit]').show();
+    $('.nextButton').hide(); 
+}
+
 var resetQuestion = function(){
     $('.message').removeClass('correct error');
     $('.message').text(' ');
-
-    console.log('reset');
-    $('input[type=submit]').show();
-    $('.nextButton').hide(); 
 }
 
 var incrementScore = function(quizState){
@@ -158,7 +129,12 @@ var incrementScore = function(quizState){
 }
 
 var resetQuiz = function(quizState){
-    quizState.questionIndex = 0;  
+    $('.results').removeClass('greenText yellowText');
+    quizState.questionIndex = 0; 
+    quizState.score = 0;  
+    document.getElementById("drumQuiz").reset();
+
+    resetQuestion();
 }
 
 $(document).ready(function(){
@@ -167,10 +143,22 @@ $(document).ready(function(){
     addQuestionProgress(quizState);
     
     $('.nextButton').on('click', function(event){   
-        resetQuestion();             
+        resetQuestion();  
+        hideNextShowSubmit();           
         renderQuiz(quizState);
         addQuestionProgress(quizState);
-    })
+    });
+
+    $('.restartQuiz').on('click', function(event){
+        resetQuiz(quizState);
+        hideNextShowSubmit(); 
+
+        $('.quizEndWrapper').fadeOut(500);
+        $('.questionWrapper ').delay(500).fadeIn();
+
+        addQuestionProgress(quizState); 
+        renderQuiz(quizState);
+    });
 
     $('#drumQuiz').submit(function(event){
         event.preventDefault();
@@ -181,7 +169,6 @@ $(document).ready(function(){
         if(selectedAnswer === correctAnswer){
             $('.message').addClass('correct').text("Correct!");
             incrementScore(quizState);
-            console.log(quizState.score);
         } else {
             $('.message').addClass('error').text("Incorrect, Please continue.");
         }
@@ -190,8 +177,8 @@ $(document).ready(function(){
         $('.nextButton').show(); 
         
         if(quizState.questionIndex === 6){
-            // resetQuiz(quizState);
             displayResults();
+            $('.nextButton').hide();
         } else {
             incrementQuestionIndex(quizState);
         }
